5aa1ddc6429ff518ff5e566117096c077380ac9a
Rick Clark <rkingsleyclark@gmail.com>
2017-01-04 15:51:10 +0000

    release bike method created

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,7 +1,6 @@
 require "dockingstation.rb"
 
 describe DockingStation do
-  it "Responds to the method release_bike" do
-    expect(DockingStation.new.respond_to?(:release_bike)).to eq true
-  end
+  docking_instance = DockingStation.new
+  it { expect(docking_instance.respond_to?(:release_bike)).to eq true }
 end
