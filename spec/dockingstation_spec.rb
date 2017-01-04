914da42d2ec33fb32952cc3b23fa5a782d2975fe
Shereen Finney <shereenfinney@gmail.com>
2017-01-04 15:42:02 +0000

    Added test to respond to release_bike method

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,7 @@
 require "dockingstation.rb"
 
 describe DockingStation do
+  it "Responds to the method release_bike" do
+    expect(DockingStation.new.respond_to?(:release_bike)).to eq true
+  end
 end
