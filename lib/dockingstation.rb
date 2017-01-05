56763490d53a9c2646333ba97414fae1511a596c
Ben Kielty <bwk103@gmail.com>
2017-01-05 17:09:44 +0000

    Refactoring methods

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -7,13 +7,23 @@ class DockingStation
   end
 
   def release_bike
-    raise "There are no docked bikes!" if @docked_bikes.empty?
+    raise "There are no docked bikes!" if empty?
     @docked_bikes.pop
   end
 
   def dock(bike)
-    raise "This docking_station is full!" if @docked_bikes.length >= 20
+    raise "This docking_station is full!" if full?
     @docked_bikes << bike
+  end
+
+private
+
+  def full?
+    @docked_bikes.length >= 20
+  end
+
+  def empty?
+    @docked_bikes.empty?
   end
 
 end
