bbac36740c2f617de5d8d10b14300a91841397ea
Rick Clark <rkingsleyclark@gmail.com>
2017-01-05 12:02:03 +0000

    bike test

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,4 +3,7 @@ require "dockingstation.rb"
 describe DockingStation do
   docking_instance = DockingStation.new
   it { expect(docking_instance.respond_to?(:release_bike)).to eq true }
+  it "can release a bike with class bike" do
+    expect(docking_instance.release_bike()).to be_instance_of(Bike)
+  end
 end
