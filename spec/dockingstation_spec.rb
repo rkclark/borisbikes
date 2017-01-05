2d4fd4650712ee2a417e8ddac5e3baa035d34f74
Rick Clark <rkingsleyclark@gmail.com>
2017-01-05 12:29:44 +0000

    dock test

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,12 +1,14 @@
 require "dockingstation.rb"
 
 describe DockingStation do
-  docking_instance = DockingStation.new
-  it { expect(docking_instance.respond_to?(:release_bike)).to eq true }
+  it { expect(subject.respond_to?(:release_bike)).to eq true }
   it "can release a bike with class bike" do
-    expect(docking_instance.release_bike()).to be_instance_of(Bike)
+    expect(subject.release_bike()).to be_instance_of(Bike)
   end
   it "expects the bike to be working" do
-    expect(docking_instance.release_bike.working?).to eq true
+    expect(subject.release_bike.working?).to eq true
+  end
+  it "#dock" do
+    expect(subject.respond_to?(:dock)).to eq true
   end
 end
